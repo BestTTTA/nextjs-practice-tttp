b66cbc84109be9d28ea9171e3e7e2f42944d4d35
BestTTTA <bst12546@gmail.com>
2025-07-06 17:18:48 +0700

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
-import Link from "next/link";
+// import Link from "next/link";
 import UseButton from "@/components/UseButton";
-import { users } from "@/lib/users";
+// import { users } from "@/lib/users";
 
 export default async function HomePage() {
   return (
     <main className="p-4">
-      <h1 className="text-xl font-bold">User lists</h1>
+
+      {/* <h1 className="text-4xl font-thin">User lists</h1>
       <ul>
         {users.map((user) => (
           <li key={user.id}>
-            <Link href={`/user/${user.id}`} className="text-blue-600 underline">
+            <Link
+              href={`/user/${user.id}`}
+              className="text-green-600 underline"
+            >
               {user.name}
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
 
       <h1 className="text-xl font-bold">Test Button</h1>
       <UseButton />
--- src/components/UseButton.tsx
+++ src/components/UseButton.tsx
@@ -3,16 +3,18 @@
 import Button from "@/app/ui/Button";
 import { useState, useEffect } from "react";
 
+import Image from "next/image";
+
 export default function UseButton() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    console.log("Effect triggered on mount");
-  }, []);
 
   const toggleButton = () => {
     setIsOpen(!isOpen);
   };
+
+  useEffect(() => {
+    console.log("Effect triggered on mount");
+  }, []);
 
   useEffect(() => {
     console.log("Button is:", isOpen ? "Open" : "Closed");
@@ -20,7 +22,21 @@ export default function UseButton() {
 
   return (
     <div className="flex flex-col gap-2">
+      <p className="text-gray-600">{isOpen.toString()}</p>
+
       <Button label={isOpen ? "Close" : "Open"} onClick={toggleButton} />
+
+      {isOpen && (
+        <div className="flex flex-col items-center">
+          <Image
+            src="/images/test.jpg"
+            alt="Next.js Logo"
+            width={200}
+            height={200}
+          />
+          <p className="text-gray-600">This is a test image</p>
+        </div>
+      )}
     </div>
   );
 }
