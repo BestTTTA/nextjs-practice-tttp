ad61d3a5b21adb290833ad26f37e3359c4b71b16
BestTTTA <bst12546@gmail.com>
2025-07-06 15:58:17 +0700

    Update

--- src/components/UseButton.tsx
+++ src/components/UseButton.tsx
@@ -4,22 +4,23 @@ import Button from "@/app/ui/Button";
 import { useState, useEffect } from "react";
 
 export default function UseButton() {
-  const [isOpen, setIsOpen] = useState<boolean>(false); 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log("Effect triggered on mount");
+  }, []);
+
+  const toggleButton = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
     console.log("Button is:", isOpen ? "Open" : "Closed");
   }, [isOpen]);
 
-  const toggleButton = () => {
-    setIsOpen(!isOpen); 
-  };
-
   return (
     <div className="flex flex-col gap-2">
-      <Button
-        label={isOpen ? "Close" : "Open"}
-        onClick={toggleButton}
-      />
+      <Button label={isOpen ? "Close" : "Open"} onClick={toggleButton} />
     </div>
   );
 }
